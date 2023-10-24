3f08a537ff9a90b2e74c42dd392db07f4153cc6b
Rafael Moreira <rafa.siffert@gmail.com>
2023-10-24 13:45:27 -0400

    done

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -22,6 +22,44 @@ def index():
 
 # Add views here
 
+@app.route('/earthquakes/<int:id>')
+def get_earthquake(id):
+    earthquake = Earthquake.query.get(id)
+    if earthquake:
+        return jsonify({
+            "id": earthquake.id,
+            "location": earthquake.location,
+            "magnitude": earthquake.magnitude,
+            "year": earthquake.year
+        })
+    else:
+        return jsonify({
+            "message": f"Earthquake {id} not found."
+        }), 404
 
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquake_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    
+    if earthquakes:
+        # If earthquakes with the specified magnitude are found
+        earthquake_data = [{
+            "id": earthquake.id,
+            "location": earthquake.location,
+            "magnitude": earthquake.magnitude,
+            "year": earthquake.year
+        } for earthquake in earthquakes]
+
+        return jsonify({
+            "count": len(earthquake_data),
+            "quakes": earthquake_data
+        }), 200
+    else:
+        # If no earthquakes with the specified magnitude are found
+        return jsonify({
+            "count": 0,
+            "quakes": []
+        }), 200
+        
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
